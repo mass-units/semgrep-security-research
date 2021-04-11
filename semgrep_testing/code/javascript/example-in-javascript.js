fc3180805a13bf1fb6129a03da8ceebedf634685
Mathew Nitz <mbnitz@icloud.com>
2021-04-11 13:11:33 -0400

    Added test_evaluate and console.logs

--- semgrep_testing/code/javascript/example-in-javascript.js
+++ semgrep_testing/code/javascript/example-in-javascript.js
@@ -1,37 +1,48 @@
 /*
 *evaluate with multiple unique conditionals to demonstrate the need for multiple rules to search for 
-*similar things
+*similar things - pattern operators have to be used in conjunction which allows us to eliminate false positives 
 */
-//in order to cover some cases your ifs need an else for sanitization
+//fastest secure coding lesson ever:
+    //in order to cover some cases your ifs need an else for sanitization
 
-function good_evaluate(value)
-{
-    var cmd = value;
-    if(cmd === "a")
-        cmd = "ls /public";
-    else if(cmd === "b")
-        cmd = "cat README";
-    else
-        cmd = "we are fine";
-    eval(cmd);
-}
-
-function bad_evaluate(value)
-{
-    var cmd = value;
-    if(cmd === "a")
-        cmd = "ls /public";
-    else if(cmd === "b")
-        cmd = "cat README";
-    eval(cmd);
-}
-
-function other__bad_evaluate(value) 
-{
-    var cmd = value;
-    if(cmd === "a")
-        cmd = "ls /public";
-    if(cmd === "b")
-        cmd = "cat README";
-    eval(cmd);
-}
+    function good_evaluate(value)
+    {
+        var cmd = value;
+        if(cmd === "a")
+            cmd = "console.log('a');";
+        else if(cmd === "b")
+            cmd = "console.log('b');";
+        else
+            cmd = "console.log('else');";
+        eval(cmd);
+    }
+    
+    function bad_evaluate(value)
+    {
+        var cmd = value;
+        if(cmd === "a")
+            cmd = "console.log('a');";
+        else if(cmd === "b")
+            cmd = "console.log('b');";
+        eval(cmd);
+    }
+    
+    function other__bad_evaluate(value) 
+    {
+        var cmd = value;
+        if(cmd === "a")
+            cmd = "console.log('a');";
+        if(cmd === "b")
+            cmd = "console.log('b');";
+        eval(cmd);
+    }
+    
+    function test_evaluate(value)
+    {
+        good_evaluate("a");
+        good_evaluate("a;console.log('evil');");
+        bad_evaluate("a");
+        bad_evaluate("a;console.log('evil');");
+        other_bad_evaluate("a");
+        other_bad_evaluate("a;console.log('evil');");
+    }
